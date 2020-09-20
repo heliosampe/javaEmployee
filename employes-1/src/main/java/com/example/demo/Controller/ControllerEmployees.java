4222c077534bc5ede927f59096933abfb39ef453
Davicho160 <62159238+Davicho160@users.noreply.github.com>
2020-09-20 18:16:36 -0500

    Update ControllerEmployees.java

--- employes-1/src/main/java/com/example/demo/Controller/ControllerEmployees.java
+++ employes-1/src/main/java/com/example/demo/Controller/ControllerEmployees.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +22,11 @@ public class ControllerEmployees {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@GetMapping({"/agregarEmpleado"})
+	@PostMapping({"/agregarEmpleado"})
 	public EmpleadoBean agregarEmpleado(@RequestBody EmpleadoBeanEntrada empleadoBeanEntrada ) {
 		EmpleadoBean response = new EmpleadoBean(); 
 		response=employeeService.guardarEmpleado(empleadoBeanEntrada);
 		return response ;
-		
 	}
 
 	
